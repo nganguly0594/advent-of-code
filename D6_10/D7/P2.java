696240496164fe7bf64b8ecf24b314c91f14b8c7
nganguly0594 <nganguly@stu.naperville203.org>
2021-12-07 01:17:46 -0600

    day 7 ez

--- D6_10/D7/P2.java
+++ D6_10/D7/P2.java
@@ -11,16 +11,16 @@ package D6_10.D7;
 import java.util.Scanner;
 import java.io.*;
 
-public class P1
+public class P2
 {
     public static void main (String[] args) throws IOException
     {
         Scanner scanfile = new Scanner(new File("texts/D7.txt"));
         String input = scanfile.nextLine().replace(",", " ");
 
-        int sum = 0;
+        long sum = 0;
         int difference = 0;
-        int lowest = 100000000;
+        long lowest = 2000000000;
         int[] array = new int[1000];
 
         Scanner scan = new Scanner(input);
@@ -35,7 +35,12 @@ public class P1
             for (int x = 0; x < array.length; x++)
             {
                 difference = Math.abs(array[x] - i);
-                sum += difference;
+                int summation = 0;
+                for (int a = 1; a <= difference; a++)
+                {
+                    summation += a;
+                }
+                sum += summation;
             }
 
             if (sum < lowest)
