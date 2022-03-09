bacc516ba54be94c18bd818a2f84f7287fc99df5
Neil Ganguly <nganguly@stu.naperville203.org>
2022-03-09 14:11:40 -0600

    dafs

--- 2021 Attempt 2/day9/day9.java
+++ 2021 Attempt 2/day9/day9.java
@@ -12,6 +12,18 @@ public class day9 {
         Scanner scanfile = new Scanner(new File("day9/input.txt"));
 
         //----------part 1----------
-        int[][] map = new int[]
+        int[][] map = new int[100][100];
+        for (int r = 0; r < 100; r++) {
+            String line = scanfile.nextLine();
+            for (int c = 0; c < 100; c++) {
+                map[r][c] = Integer.parseInt(line.substring(c, c + 1));
+            }
+        }
+        
+        for (int r = 0; r < 100; r++) {
+            for (int c = 0; c < 100; c++) {
+                System.out.println(map[r][c]);
+            }
+        }
     }
 }
