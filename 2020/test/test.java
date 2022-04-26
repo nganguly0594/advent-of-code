b78ed50fc82bdece95d90cf9a5fbe5d4bf7c9b6f
Neil Ganguly <nganguly@stu.naperville203.org>
2022-04-26 09:09:35 -0500

    blah

--- 2020/test/test.java
+++ 2020/test/test.java
@@ -8,29 +8,15 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println(scrambleWord("TAN"));
-        System.out.println(scrambleWord("ABRACADABRA"));
-        System.out.println(scrambleWord("WHOA"));
-        System.out.println(scrambleWord("AARDVARK"));
-        System.out.println(scrambleWord("EGGS"));
-        System.out.println(scrambleWord("A"));
-        System.out.println(scrambleWord(""));
+        System.out.println(summation(20));
     }
 
-    public static String scrambleWord(String word) {
-        String scramble = "";
-        for (int i = 0; i < word.length() - 1; i++) {
-            if (word.charAt(i) == 'A' && word.charAt(i + 1) != 'A') {
-                scramble += word.substring(i + 1, i + 2) + word.substring(i, i + 1);
-                i++;
-            }
-            else {
-                scramble += word.substring(i, i + 1);
-            }
+    public static long summation(long num) {
+        if (num == 1) {
+            return 1;
         }
-        if (scramble.length() != word.length()) {
-            scramble += word.substring(word.length() - 1, word.length());
+        else {
+            return num + summation(num - 1);
         }
-        return scramble;
     }
 }
