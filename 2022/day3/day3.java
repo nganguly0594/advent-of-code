4e04e69c0fbf1bbb4e18684f52bb7d0d60e21623
Neil Ganguly <nganguly@stu.naperville203.org>
2022-12-02 08:20:18 -0600

    fd

---
+++ 2022/day3/day3.java
@@ -0,0 +1,24 @@
+package day3;
+
+/**
+ * day3
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class day3 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanfile = new Scanner(new File("input.txt"));
+        ArrayList<String> inputs = new ArrayList<>();
+        while (scanfile.hasNext()) {
+            inputs.add(scanfile.nextLine());
+        }
+
+        //----------part 1----------
+
+
+
+        //----------part 2----------
+    }
+}
