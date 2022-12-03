3673045a76caf3233a8b099e0d08ebcaf8a2df39
Neil Ganguly <nganguly@stu.naperville203.org>
2022-12-02 23:37:14 -0600

    hui

---
+++ 2022/day2/day2.java
@@ -0,0 +1,109 @@
+package day2;
+
+/**
+ * day2
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class day2 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanfile = new Scanner(new File("day2/input.txt"));
+
+        //----------part 1----------
+        ArrayList<Character> opponent = new ArrayList<>();
+        ArrayList<Character> guide = new ArrayList<>();
+
+        while (scanfile.hasNext()) {
+            opponent.add(scanfile.next().charAt(0));
+            char c = scanfile.next().charAt(0);
+            if (c == 'X') {
+                guide.add('A');
+            }
+            else if (c == 'Y') {
+                guide.add('B');
+            }
+            else {
+                guide.add('C');
+            }
+        }
+
+        int totalScore = 0;
+
+        for (int i = 0; i < opponent.size(); i++) {
+            totalScore += score(opponent.get(i), guide.get(i));
+        }
+
+        System.out.println("Part 1 answer: " + totalScore);
+
+        //----------part 2----------
+        
+        totalScore = 0;
+
+        for (int i = 0; i < opponent.size(); i++) {
+            char c = opponent.get(i);
+
+            if (guide.get(i) == 'C') {
+                totalScore += 6;
+                if (c == 'A') {
+                    totalScore += 2;
+                }
+                else if (c == 'B') {
+                    totalScore += 3;
+                }
+                else {
+                    totalScore += 1;
+                }
+            }
+            else if (guide.get(i) == 'B') {
+                totalScore += 3;
+                if (c == 'A') {
+                    totalScore += 1;
+                }
+                else if (c == 'B') {
+                    totalScore += 2;
+                }
+                else {
+                    totalScore += 3;
+                }
+            }
+            else {
+                if (c == 'A') {
+                    totalScore += 3;
+                }
+                else if (c == 'B') {
+                    totalScore += 1;
+                }
+                else {
+                    totalScore += 2;
+                }
+            }
+        }
+
+        System.out.println("Part 2 answer: " + totalScore);
+    }
+
+    public static int score(char one, char two) {
+        int sum = 0;
+        if (two == 'A') {
+            sum += 1;
+        }
+        else if (two == 'B') {
+            sum += 2;
+        }
+        else {
+            sum += 3;
+        }
+
+        if (one == two) {
+            sum += 3;
+        }
+        else if ((one == 'A' && two == 'B') || (one == 'B' && two == 'C') || (one == 'C' && two == 'A')) {
+            sum += 6;
+        }
+        
+        return sum;
+    }
+}
---
+++ 2022/day3/day3.java
@@ -0,0 +1,64 @@
+package day3;
+
+/*
+ * day3
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class day3 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanfile = new Scanner(new File("day3/input.txt"));
+        ArrayList<String> inputs = new ArrayList<>();
+        while (scanfile.hasNext()) {
+            inputs.add(scanfile.nextLine());
+        }
+
+        // ----------part 1----------
+        int prioritySum = 0;
+
+        for (String s : inputs) {
+            String one = s.substring(0, (s.length() / 2));
+            String two = s.substring(s.length() / 2);
+            
+            for (int i = 0; i < one.length(); i++) {
+                String letter = one.substring(i, i + 1);
+                if (two.contains(letter)) {
+                    prioritySum += priority(letter);
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Part 1 Answer: " + prioritySum);
+
+        // ----------part 2----------
+        prioritySum = 0;
+
+        for (int i = 0; i < inputs.size(); i += 3) {
+            String one = inputs.get(i);
+            String two = inputs.get(i + 1);
+            String three = inputs.get(i + 2);
+
+            for (int j = 0; j < one.length(); j++) {
+                String letter = one.substring(j, j + 1);
+                if (two.contains(letter) && three.contains(letter)) {
+                    prioritySum += priority(letter);
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Part 2 Answer: " + prioritySum);
+    }
+
+    public static int priority(String stuff) {
+        char prior = stuff.charAt(0);
+        if (Character.isUpperCase(prior)) {
+            return prior - 38;
+        } else {
+            return prior - 96;
+        }
+    }
+}
