256dc67fb27af68f072e42d0b0c9e364d67bc309
Neil Ganguly <nganguly@stu.naperville203.org>
2022-12-10 00:59:42 -0600

    dsfa

--- 2022/day10/day10.java
+++ 2022/day10/day10.java
@@ -15,13 +15,50 @@ public class day10 {
             inputs.add(scanfile.nextLine());
         }
 
-        //----------part 1----------
+        //----------part 1 and part 2----------
+        int sum = 0;
+        int x = 1;
+        int cycle = 0;
+        int i = 0;
+        boolean first = true;
+        String output = "";
 
-        
-        System.out.println("Part 1 Answer: ");
+        while (i < inputs.size()) {
+            if (Math.abs(x - (cycle % 40)) <= 1) {
+                output += "#";
+            }
+            else {
+                output += ".";
+            }
+            
+            cycle++;
 
+            if (cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220) {
+                sum += (x * cycle);
+            }
+            
+            String[] parts = inputs.get(i).split(" ");
 
-        //----------part 2----------
+            if (parts[0].equals("noop")) {
+                i++;
+            }
+            else {
+                if (first) {
+                    first = false;
+                }
+                else {
+                    x += Integer.parseInt(parts[1]);
+                    first = true;
+                    i++;
+                }
+            }
+        }
+
+        System.out.println("Part 1 Answer: " + sum);
         System.out.println("Part 2 Answer: ");
+
+        for (int index = 0; index < output.length(); index += 40) {
+            System.out.println(output.substring(index, index + 40));
+        }
     }
 }
