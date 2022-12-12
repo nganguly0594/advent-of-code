199f899feaf912b228e0a4448ae9605af487f6bc
Neil Ganguly <nganguly@stu.naperville203.org>
2022-12-12 02:43:59 -0600

    iyg

---
+++ 2022/day12/Point.java
@@ -0,0 +1,21 @@
+package day12;
+
+public class Point {
+    public int row;
+    public int col;
+    public Point prev;
+    public char val;
+    public int distance;
+
+    public Point(int r, int c, char v, int dist) {
+        row = r;
+        col = c;
+        prev = null;
+        val = v;
+        distance = dist;
+    }
+
+    public String toString() {
+        return "Row: " + row + " Col: " + col;
+    }
+}
--- 2022/day12/day12.java
+++ 2022/day12/day12.java
@@ -8,6 +8,10 @@ import java.io.*;
 import java.util.*;
 
 public class day12 {
+    public static Point start;
+    public static Point curr;
+    public static Point[][] grid;
+
     public static void main(String[] args) throws IOException {
         Scanner scanfile = new Scanner(new File("day12/input.txt"));
         ArrayList<String> inputs = new ArrayList<>();
@@ -15,13 +19,115 @@ public class day12 {
             inputs.add(scanfile.nextLine());
         }
 
-        //----------part 1----------
+        grid = new Point[inputs.size()][inputs.get(0).length()];
 
-        
-        System.out.println("Part 1 Answer: ");
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                Point temp = new Point(r, c, inputs.get(r).charAt(c), Integer.MAX_VALUE);
+                if (temp.val == 'S') {
+                    start = temp;
+                    start.val = 96;
+                    start.distance = 0;
+                    grid[r][c] = start;
+                }
+                else if (temp.val == 'E') {
+                    temp.val = 123;
+                    grid[r][c] = temp;
+                }
+                else {
+                    grid[r][c] = temp;
+                }
+            }
+        }
 
+        // ----------part 1----------
+        Queue<Point> worklist = new LinkedList<>();
+        worklist.add(start);
 
-        //----------part 2----------
+        int steps = 0;
+
+        while (!worklist.isEmpty()) {
+            curr = worklist.remove();
+
+            if (curr.val == 123) {
+                Point test = curr;
+                while (test.prev != null) {
+                    steps++;
+                    test = test.prev;
+                }
+                break;
+            }
+
+            ArrayList<Point> neighbors = getNeighbors(curr);
+
+            for (int i = 0; i < neighbors.size(); i++) {
+                Point explore = neighbors.get(i);
+                int dist = curr.distance + 1;
+                if (explore.val > curr.val + 1) {
+                    continue;
+                }
+                if (dist < explore.distance) {
+                    explore.prev = curr;
+                    explore.distance = dist;
+                    worklist.add(explore);
+                }
+            }
+            /*
+            for (int i = 0; i < neighbors.size(); i++) {
+                Point explore = neighbors.get(i);
+                if (explore.state != 1 && (curr.val == 'S' || explore.val <= curr.val + 1)) {
+                    explore.state = 1;
+                    if (curr.val == 'S' || explore.val == '{' || explore.val <= curr.val + 1) {
+                        explore.prev = curr;
+                        explore.distance = curr.distance + 1;
+                        worklist.add(explore);
+                    }
+                }
+            }
+            */
+        }
+
+        System.out.println(getPath());
+
+        System.out.println("Part 1 Answer: " + steps);
+
+        // ----------part 2----------
         System.out.println("Part 2 Answer: ");
+    }
+
+    public static ArrayList<Point> getNeighbors(Point p) {
+        ArrayList<Point> around = new ArrayList<>();
+        if (p.row - 1 >= 0) {
+            around.add(grid[p.row - 1][p.col]);
+        }
+        if (p.row + 1 < grid.length) {
+            around.add(grid[p.row + 1][p.col]);
+        }
+        if (p.col - 1 >= 0) {
+            around.add(grid[p.row][p.col - 1]);
+        }
+        if (p.col + 1 < grid[0].length) {
+            around.add(grid[p.row][p.col + 1]);
+        }
+
+        return around;
+    }
+
+    public static String getPath() {
+        String path = "";
+
+        Stack<String> stackPath = new Stack<String>();
+        Point sq = curr;
+
+        while (sq != null) {
+            stackPath.push("[" + sq.row + ", " + sq.col + "]");
+            sq = sq.prev;
+        }
+
+        while (!stackPath.isEmpty()) {
+            path += stackPath.pop();
+        }
+
+        return path;
     }
 }
