f8e4f95a344e298063c149d5250d447eeec6e804
nganguly0594 <neilganguly2015@gmail.com>
2024-12-06 23:52:52 -0600

    day 7

--- 2024/day7/solution.py
+++ 2024/day7/solution.py
@@ -1,5 +1,42 @@
-with open("day7/input.txt", "r") as file:
-    lines = file.read().strip()
+totals = []
+all_terms = []
 
-print("Part 1 Solution:")
-print("Part 2 Solution:")
+with open("day7/input.txt", "r") as file:
+    for line in file.read().splitlines():
+        total, terms = line.split(": ")
+        totals.append(int(total))
+        all_terms.append(list(map(int, terms.split())))
+
+
+def backtrack1(index, curr_total, total, terms):
+    if index == len(terms):
+        return curr_total == total
+
+    return backtrack1(index + 1, curr_total + terms[index], total, terms) or backtrack1(
+        index + 1, curr_total * terms[index], total, terms
+    )
+
+
+def backtrack2(index, curr_total, total, terms):
+    if index == len(terms):
+        return curr_total == total
+
+    return (
+        backtrack2(index + 1, curr_total + terms[index], total, terms)
+        or backtrack2(index + 1, curr_total * terms[index], total, terms)
+        or backtrack2(index + 1, int(str(curr_total) + str(terms[index])), total, terms)
+    )
+
+
+calibration1 = 0
+calibration2 = 0
+
+for total, terms in zip(totals, all_terms):
+    if backtrack1(1, terms[0], total, terms):
+        calibration1 += total
+    if backtrack2(1, terms[0], total, terms):
+        calibration2 += total
+
+
+print("Part 1 Solution:", calibration1)
+print("Part 2 Solution:", calibration2)
