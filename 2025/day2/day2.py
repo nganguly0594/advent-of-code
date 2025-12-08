b678454285eabe2891e77bb63142d91668d56146
nganguly0594 <neilganguly2015@gmail.com>
2025-12-07 19:11:15 -0600

    day 2 done

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -1,48 +1,60 @@
 def parse(data):
-    nums = []
+    ranges = []
 
-    for line in data:
-        num = int(line[1:])
-        nums.append((-1 if line[0] == 'L' else 1, num))
+    for range in data[0].split(','):
+        start, end = map(int, range.split('-'))
+        ranges.append((start, end))
     
-    return nums
+    return ranges
 
 def part1(data):
-    ret = 0
-    curr = 50
+    total = 0
 
-    for sign, num in data:
-        num %= 100
-        curr = (curr + sign * num) % 100
+    for start, end in data:
+        for i in range(start, end + 1):
+            curr = str(i)
+            half = len(curr) // 2
+            if len(curr) - half != half:
+                continue
         
-        if not curr:
-            ret += 1
+            if curr[:half] == curr[half:]:
+                total += i
     
-    return ret
+    return total
 
 def part2(data):
-    ret = 0
-    curr = 50
+    total = 0
 
-    for sign, num in data:
-        ret += num // 100
-        num %= 100
+    for start, end in data:
+        for i in range(start, end + 1):
+            curr = str(i)
 
-        prev = curr
-        curr += sign * num
+            repeats = False
 
-        if (prev and (curr < 0 or curr > 99)):
-            ret += 1
-        
-        if not curr:
-            ret += 1
-        
-        curr %= 100
+            for div in range(2, len(curr) + 1):
+                if len(curr) % div != 0:
+                    continue
+            
+                small_len = len(curr) // div
+
+                same = True
+
+                for j in range(0, len(curr) - small_len, small_len):
+                    if curr[j:j + small_len] != curr[j + small_len:j + 2 * small_len]:
+                        same = False
+                        break
+                
+                if same:
+                    repeats = True
+                    break
+            
+            if repeats:
+                total += i
     
-    return ret
+    return total
 
 if __name__ == "__main__":
-    with open("day2/test2.txt") as f:
+    with open("day2/input2.txt") as f:
         data = f.read().splitlines()
     
     data = parse(data)
