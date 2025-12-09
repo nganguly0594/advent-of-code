80283e69d6c89b469992bd879f9f965f48bf7415
nganguly0594 <neilganguly2015@gmail.com>
2025-12-08 22:34:07 -0600

    24

--- 2024/day9/day9.py
+++ 2024/day9/day9.py
@@ -1,14 +1,32 @@
 def parse(data):
-    return data
+    return data[0]
 
 def part1(data):
-    pass
+    memory = data
+    left, right = 0, len(memory)
+
+    end_id = len(memory) // 2
+    
+    check_sum = 0
+    free = 0
+    curr_id = 0
+    true_idx = 0
+
+    while left < right or (left == right and not free):
+        block = int(memory[left])
+
+        if not free:
+            for i in range(true_idx):
+                continue
+
+        free ^= 1
+
 
 def part2(data):
     pass
 
 if __name__ == '__main__':
-    with open('day9/input9.txt') as f:
+    with open('day9/test9.txt') as f:
         data = f.read().splitlines()
     
     data = parse(data)
