25dadf9214cfd8404b5b5e7c6ffe69d6e5109164
nganguly0594 <neilganguly2015@gmail.com>
2025-12-11 23:47:29 -0600

    finished 2025

--- 2025/day12/day12.py
+++ 2025/day12/day12.py
@@ -1,16 +1,32 @@
 def parse(data):
-    return data
+    *shapes, areas = data.split('\n\n')
+
+    sizes = [sum(c == '#' for c in shape) for shape in shapes]
+
+    return sizes, areas
 
 def part1(data):
-    pass
+    sizes, areas = data
 
-def part2(data):
-    pass
+    possible = 0
+
+    for line in areas.splitlines():
+        dims, *counts = line.split()
+        a, b = tuple(map(int, dims[:-1].split('x')))
+        available = a * b
+        required = 0
+
+        for i, count in enumerate(map(int, counts)):
+            required += count * sizes[i]
+        
+        if required <= available:
+            possible += 1
+    
+    return possible
 
 if __name__ == "__main__":
-    with open("day10/test10.txt") as f:
-        data = f.read().splitlines()
+    with open("day12/input12.txt") as f:
+        data = f.read()
     
     data = parse(data)
     print(part1(data))
-    print(part2(data))
